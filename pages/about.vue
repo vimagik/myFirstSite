4bae5fa7f64429e002271a3f5f374a2ca6d0f9fd
Rzhavin Georgii <rzhavin@my.com>
2021-11-20 18:00:36 +0300

    added about page with new fichers

--- pages/about.vue
+++ pages/about.vue
@@ -1,13 +1,77 @@
 <template>
-  <div>Об авторе</div>
+  <div :style="{'background-image': 'url(' + backgroundImage + ')'}" class="FtMainBody">
+    <v-container>
+      <v-row>
+        <v-spacer />
+        <v-col align="center">
+          <v-img :src="authorLogo" max-width="332" @mouseenter="changeToNew" @mouseleave="changeToOld" />
+        </v-col>
+        <v-spacer />
+      </v-row>
+      <v-row>
+        <v-col align="center">
+          <div class="text-body-1 ft-main-text">
+            <p>Привет!</p>
+            <p>Хотите побольше обо мне узнать? Ладно!</p>
+            <p>Зовут меня Ржавин Георгий или просто Жора. Мне 15 лет. Родился 1 августа 1984 года.</p>
+            <p>
+              Учусь в школе N 18
+              города Йошкар-Ола, в 10 &quot;А&quot; классе. My hobby is мой
+              компьютер. Люблю вкусно поесть.
+            </p>
+            <p>Ну и все пожалуй.</p>
+          </div>
+
+          <v-img
+            :src="backButton"
+            max-width="113"
+            class="ft-button"
+            @click="$router.go(-1)"
+          />
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
 </template>
 
 <script>
+import backgroundImage from '@/static/dob.jpg'
+import authorImage from '@/static/georgemr.jpg'
+import backButton from '@/static/backButton.gif'
+import authorImageNew from '@/static/georgemr_new.jpg'
+
 export default {
-  name: 'About'
+  name: 'About',
+  data () {
+    return {
+      backgroundImage,
+      authorLogo: authorImage,
+      backButton
+    }
+  },
+  methods: {
+    changeToNew () {
+      this.authorLogo = authorImageNew
+    },
+    changeToOld () {
+      this.authorLogo = authorImage
+    }
+  }
 }
 </script>
 
 <style scoped>
+.FtMainBody {
+  background-repeat: repeat;
+  height: 100%;
+}
+
+.ft-button {
+  cursor: pointer;
+}
+
+.ft-main-text {
+  color: blue;
+}
 
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -19,18 +19,53 @@
           </div>
         </v-col>
       </v-row>
+      <v-row>
+        <v-col>
+          <v-btn outlined color="indigo" @click="dialog = true">
+            Реставрация сайта 20.11.2021
+          </v-btn>
+        </v-col>
+      </v-row>
+      <v-row justify="center">
+        <v-dialog v-model="dialog" max-width="490" persistent>
+          <v-card>
+            <v-img :src="nuxtLogo" max-width="490" />
+            <v-card-text class="mt-6">
+              <p>
+                Мой первый сайт я создал и разместил более 20 лет назад. Много воды утекло, а он до сих пор доступен по
+                адресу: <a href="http://georgemr.chat.ru/" target="_blank">http://georgemr.chat.ru/</a>
+              </p>
+              <p>
+                Правда часть функционала уже утрачена, поэтому пришла идея отреставрировать сайт с применением
+                современных технологиях: SPA, VUE, NUXT.
+              </p>
+              <p>Дизайн и наполнение восстановлены без изменений.</p>
+            </v-card-text>
+            <v-card-actions>
+              <v-spacer />
+              <v-btn @click="dialog = false">
+                Принято
+              </v-btn>
+              <v-spacer />
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+      </v-row>
     </v-container>
   </div>
 </template>
 
 <script>
 import mainbgimage from '@/static/bgmainpage.jpeg'
+import nuxtLogo from '@/static/nuxt.png'
 
 export default {
   name: 'Index',
   data () {
     return {
-      mainBgImage: mainbgimage
+      mainBgImage: mainbgimage,
+      dialog: false,
+      nuxtLogo
     }
   }
 }
