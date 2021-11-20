a77c03908f67f032dc8c330865b2c27d42e173a5
UbuntuX <vimagik@gmail.com>
2021-11-20 13:57:54 +0300

    Added index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,48 @@
 <template>
-  <div class="text--primary">
-    Наполнение
+  <div :style="{'background-image': 'url(' + mainBgImage + ')'}" class="FtMainBody">
+    <v-container>
+      <v-row>
+        <v-col>
+          <div class="text--primary">
+            <p class="ft-main-text">
+              Приветствую вас! В данный момент вы находитесь на страничке &quot;Фортуна&quot;. Здесь вы можете узнать
+              много об
+              Экспериментальной школе-комплекс N 18 города Йошкар-Ола. Так же вы можете расширить не только круг друзей,
+              но и собственный кругозор.От души посмеяться!!!
+            </p>
+            <p class="ft-main-text">
+              Рекомендую для лучшего просмотра использовать <span class="black--text">Netscape Communicator.</span>
+            </p>
+            <p>
+              Последнее обновление 20.02.2000
+            </p>
+          </div>
+        </v-col>
+      </v-row>
+    </v-container>
   </div>
 </template>
+
+<script>
+import mainbgimage from '@/static/bgmainpage.jpeg'
+
+export default {
+  name: 'Index',
+  data () {
+    return {
+      mainBgImage: mainbgimage
+    }
+  }
+}
+</script>
+
+<style>
+.ft-main-text {
+  color: blue;
+}
+
+.FtMainBody {
+  background-repeat: repeat;
+  height: 100%;
+}
+</style>
